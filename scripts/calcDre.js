358ded3964d89e38e6500a634edd55f31b20177a
Isaac Magalhaes <isaacmagalhaesdeoliveira@gmail.com>
2023-06-05 00:28:57 -0300

    Foram mudados alguns conceitos.

---
+++ scripts/calcDre.js
@@ -0,0 +1,26 @@
+    let rb = document.querySelector('.rb');
+    let dd = document.querySelector('.dd');
+    let cc = document.querySelector('.cc');
+    let dv = document.querySelector('.dv');
+    let da = document.querySelector('.da');
+    let df = document.querySelector('.df');
+    let pic = document.querySelector('.pic');
+    let resultado_liquido = document.querySelector('h2');
+     
+
+function calcular_dre() {
+     resultado_liquido.innerHTML = parseFloat(rb.value) - parseFloat(dd.value) - parseFloat(cc.value) - parseFloat(dv.value) - parseFloat(da.value) - parseFloat(df.value) - parseFloat(pic.value)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
